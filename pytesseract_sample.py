7d2e180111f0d14c4ad83cb5765be7dc0f84153a
RohYounwoo <ywroh0619@naver.com>
2020-05-14 20:28:29 +0900

    create_\sample

--- pytesseract_sample.py
+++ pytesseract_sample.py
@@ -1,8 +1,8 @@
 import pytesseract as tess
 tess.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
-from PIL import Image
+import cv2
 
-img = Image.open('./image/anything.jpg')
+img = cv2.imread('./image/anything.jpg')
 text = tess.image_to_string(img)
 
 
