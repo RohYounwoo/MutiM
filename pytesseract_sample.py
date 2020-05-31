6dbfcedd5673ab896e0a33e6faa275170480a84d
RohYounwoo <ywroh0619@naver.com>
2020-05-31 19:26:46 +0900

    improve text crop

--- pytesseract_sample.py
+++ pytesseract_sample.py
@@ -6,7 +6,11 @@ import cv2
 def readtext(src):
     img = cv2.imread(src, cv2.IMREAD_GRAYSCALE)
     HalfHeight = int(len(img[:,1])/2)
-    text = tess.image_to_string(img[HalfHeight:, :])
+    img = img[HalfHeight:, :]
+
+    #la_edge_img = cv2.Laplacian(img, -1)
+    
+    text = tess.image_to_string(img)
     print(text)
 
 
